0fc77e1b880e633be46b91b00d6b9363f945bcbe
26nch <nchb1807838@gmail.com>
2025-09-03 15:52:26 +0700

    df config alb

---
+++ modules/alb/main.tf
@@ -0,0 +1,85 @@
+# security group for ALB
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.alb_name}-sg-${var.env}"
+  description = "Security group for ALB"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "HTTP from Internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS from Internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.alb_name}-sg-${var.env}"
+  }
+}
+
+# create ALB
+resource "aws_lb" "main" {
+  name               = "${var.alb_name}-${var.env}"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = var.subnet_ids
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "${var.alb_name}-${var.env}"
+  }
+}
+
+# create target group
+resource "aws_lb_target_group" "main" {
+  name     = "${var.alb_name}-tg-${var.env}"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "${var.alb_name}-tg-${var.env}"
+  }
+}
+
+# HTTP Listener
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
---
+++ modules/alb/variables.tf
@@ -0,0 +1,19 @@
+variable "env" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for ALB"
+  type        = list(string)
+}
+
+variable "alb_name" {
+  description = "Name of the ALB"
+  type        = string
+}
