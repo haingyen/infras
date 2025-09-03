62013b2d6ab763a865801545a4fc91d5a8e85b3d
26nch <nchb1807838@gmail.com>
2025-09-03 15:54:19 +0700

    add alb module

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,3 +1,4 @@
+# Deploy VPC module
 module "vpc" {
   source = "../../modules/vpc"
 
@@ -6,5 +7,15 @@ module "vpc" {
   public_subnet_cidrs    = var.public_subnet_cidrs
   private_subnet_cidrs   = var.private_subnet_cidrs
   availability_zones     = var.availability_zones
-  env                    = "dev"
+  env                    = var.env
+}
+
+# Deploy ALB module
+module "alb" {
+  source = "../../modules/alb"
+
+  env        = var.env
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnet_ids
+  alb_name   = "nginx-alb"
 }
