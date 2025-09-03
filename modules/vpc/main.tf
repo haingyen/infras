3b597b59a6c683f465fc9bea29cb36547bf90c2a
26nch <nchb1807838@gmail.com>
2025-09-03 10:45:22 +0700

    mdf module vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,117 +1,57 @@
-
+# Main VPC Configuration
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
-
+  cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support = true
 
   tags = {
     Name = var.vpc_name
+    Environment = var.env
   }
 }
 
-resource "aws_subnet" "public_subnet_1" {
+# Internet Gateway
+resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
-  cidr_block = var.cidr_block_public_subnet_1
-  availability_zone = var.availability_zone
-  map_public_ip_on_launch = true
-
   tags = {
-    Name = var.public_subnet_name
+    Name = "${var.vpc_name}-igw"
+    Environment = var.env
   }
 }
 
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id = aws_vpc.main.id
+# Public Subnets
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
 
-  cidr_block = var.cidr_block_private_subnet_1
-  availability_zone = var.availability_zone
-  map_public_ip_on_launch = true
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = element(var.availability_zones, count.index)
 
   tags = {
-    Name = var.private_subnet_name
+    Name        = "${var.vpc_name}-public-${count.index + 1}"
+    Environment = var.env
   }
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = var.igw_name
-  }
-}
-
-#==============NAT-GATEWAY======================#
-resource "aws_eip" "nat_eip" {
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id = aws_subnet.public_subnet_1.id
-  tags = {
-    Name = var.nat_gw_name
-  }
-}
-
-# ROUTE TABLES
-resource "aws_route_table" "public_rt" {
+# Route table
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = var.cidr_public_rt
-    gateway_id = aws_internet_gateway.igw.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
   }
-
   tags = {
-    Name = var.public_rt_name
+    Name = "${var.vpc_name}-public-rt"
+    Environment = var.env
   }
 }
 
-resource "aws_route_table_association" "public_rt_asso" {
-  route_table_id = aws_route_table.public_rt.id
-  subnet_id = aws_subnet.public_subnet_1.id
-}
+# association
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
 
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = var.cidr_private_rt
-    gateway_id = aws_nat_gateway.nat_gw.id
-  }
-
-  tags = {
-    Name = var.private_rt_name
-  }
-}
-
-resource "aws_security_group" "web_sg" {
-  description = "Allow SSH and HTTP"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Cho phép HTTP
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  # Cho phép mọi outbound traffic
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = var.web_sg
-  }
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
